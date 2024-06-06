25de4f691b5f70582d387cf5826a38c3d85ca069
alstjs37 <kubernetes.io@kakao.com>
2024-06-06 15:28:39 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int up = in.nextInt();
+		int down = in.nextInt();
+		int length = in.nextInt();
+ 
+		int day = (length - down) / (up - down);
+		if ((length - down) % (up - down) != 0) {
+			day++;
+		}
+		System.out.println(day);
+	}
+}
