e7e0c4e63ee17d7932965ef6ed421cb808a9bf38
alstjs37 <kubernetes.io@kakao.com>
2024-06-12 13:42:26 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
 select a.id, a.email, a.first_name, a.last_name
 from developer_infos a
-where a.skill_1 = 'Python' or a.skill_2 = 'Python' or a.skill_3 = 'Python'
+where 'Python' in (a.skill_1, a.skill_2, a.skill_3)
 order by a.id asc;
